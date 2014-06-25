ec450950428f4fc207727f503115461fef07dc7d
Matthew Craig <mattwcraig@gmail.com>
2014-06-25 12:08:10 -0400

    Add sample metadata file function to get its location

--- reducer/notebook_dir.py
+++ reducer/notebook_dir.py
@@ -19,3 +19,17 @@ def get_notebook_path():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     notebook_path = os.path.join(this_dir, notebook_name)
     return notebook_path
+
+
+def get_data_path():
+    """
+    Return the absolute path to the folder containing package data.
+
+    Returns
+    -------
+    str
+        Name of path
+    """
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(this_dir, 'data')
+    return data_dir
