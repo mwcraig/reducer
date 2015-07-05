e69402647f0d3878ced159a3e8f7dbc386a8158f
Matthew Craig <mattwcraig@gmail.com>
2015-07-04 20:26:48 -0500

    Make version match that from setup.py

--- reducer/__init__.py
+++ reducer/__init__.py
@@ -1,6 +1,6 @@
 # keep this at the top -- name is needed for imports to succeed
 NOTEBOOK_TEMPLATE_NAME = 'reducer-template.ipynb'
 
-__version__ = '0.1.dev8'
+__version__ = '0.1.dev9'
 
 from .core import *
